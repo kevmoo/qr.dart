2765a602ceecbdcb4efc8e867671ff33e84a70bf
Kevin Moore <kevin@thinkpixellab.com>
2012-05-08 15:45:43 -0700

    use simple html config

--- tests/__runner.dart
+++ tests/__runner.dart
@@ -1,8 +1,8 @@
-#import('/Users/kevin/Downloads/dart/dart-sdk/lib/dart2js/lib/unittest/html_enhanced_config.dart');
+#import('/Users/kevin/Downloads/dart/dart-sdk/lib/dart2js/lib/unittest/html_config.dart');
 #import('_tests.dart');
 
 main() {
-  useHtmlEnhancedConfiguration();
+  useHtmlConfiguration();
 
   test_hello_world.run();
 }
