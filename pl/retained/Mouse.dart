a3cf4ef60724fbc7b4265032d3fcd72c917b998c
Kevin Moore <kevin@thinkpixellab.com>
2012-05-25 16:09:57 -0700

    default values are nice

--- pl/retained/Mouse.dart
+++ pl/retained/Mouse.dart
@@ -1,6 +1,6 @@
 class Mouse {
-  static final Property _isMouseOverProperty = const Property("IsMouseOver");
-  static final Property _isMouseDirectlyOverProperty = const Property("IsMouseDirectlyOver");
+  static final Property _isMouseOverProperty = const Property("IsMouseOver", false);
+  static final Property _isMouseDirectlyOverProperty = const Property("IsMouseDirectlyOver", false);
   static final Property _stageMouseCacheProperty = const Property("_stageMouseCacheProperty");
 
   static bool isMouseOver(PElement element){
