235a3bfc8b4a31dab5f366ed8a9a982cf14d4d4f
Kevin Moore <kevin@thinkpixellab.com>
2012-05-29 14:18:47 -0700

    first attempt at property listener logic

--- src/properties/Property.dart
+++ src/properties/Property.dart
@@ -33,25 +33,106 @@ class Property<T> implements Hashable{
   void set(IPropertyObject obj, T value){
     assert(value != Undefined);
     obj.propertyValues._set(this, value);
+    _PropertyChangeHelper.fireHandlers(obj, this);
   }
 
   void clear(IPropertyObject obj){
     obj.propertyValues._remove(this);
+    _PropertyChangeHelper.fireHandlers(obj, this);
   }
 
   bool isSet(IPropertyObject obj){
     return obj.propertyValues._isSet(this);
   }
 
+  void addHandler(IPropertyObject obj, PropertyChangedHandler handler){
+    _PropertyChangeHelper.addHandler(obj, this, handler);
   }
+  
+  void removeHandler(IPropertyObject obj, PropertyChangedHandler handler){
+    _PropertyChangeHelper.removeHandler(obj, this, handler);
+  }  
 
   int hashCode(){
     return _id;
   }
 }
 
+typedef void PropertyChangedHandler(IPropertyObject obj, Property property);
+
 class _UndefinedValue{
   const _UndefinedValue();
   // TODO: toString?
 }
 
+class _PropertyChangeHelper{
+  // TODO: once we can define static final with 'new' instead of 'const', we can nuke the property redirection
+  static Property<_PropertyChangeHelper> __changeHelperProperty;
+
+  final HashMap<Property, _SimpleSet<PropertyChangedHandler>> _handlers;
+
+  _PropertyChangeHelper() : _handlers = new HashMap<Property, _SimpleSet<PropertyChangedHandler>>();
+
+
+  static Property<_PropertyChangeHelper> get _changeHelperProperty(){
+    if(__changeHelperProperty == null){
+      __changeHelperProperty = new Property<_PropertyChangeHelper>.withFactory("_changeHelperProperty", createInstance);
+    }
+    return __changeHelperProperty;
+  }
+
+  static _PropertyChangeHelper createInstance(){
+    return new _PropertyChangeHelper();
+  }
+
+  static void addHandler(IPropertyObject obj, Property property, PropertyChangedHandler watcher){
+    var helper = _changeHelperProperty.get(obj);
+    helper.getSet(property).add(watcher);
+  }
+
+  static void removeHandler(IPropertyObject obj, Property property, PropertyChangedHandler watcher){
+    var helper = _changeHelperProperty.get(obj);
+    helper.getSet(property).remove(watcher);
+  }
+
+  static void fireHandlers(IPropertyObject obj, Property property){
+    var helper = _changeHelperProperty.get(obj);
+    var theSet = helper.getSet(property);
+    theSet.forEach((h){
+      h(obj, property);
+    });
+  }
+
+  _SimpleSet<PropertyChangedHandler> getSet(Property property){
+    return _handlers.putIfAbsent(property, propSetCreator);
+  }
+
+  static _SimpleSet<PropertyChangedHandler> propSetCreator(){
+    return new _SimpleSet<PropertyChangedHandler>();
+  }
+}
+
+class _SimpleSet<T>{
+  final List<T> _items;
+
+  _SimpleSet():_items = new List<T>();
+
+  // NOTE: this is not 100% set-ish. 'add' should just ignore duplicates, not assert
+  void add(T item){
+    assert(_items.indexOf(item) == -1);
+    _items.add(item);
+  }
+  
+  bool remove(T item){
+    var index = _items.indexOf(item);
+    if(index >= 0){
+      _items.removeRange(index, 1);
+      return true;
+    }
+    return false;
+  }
+
+  void forEach(void f(T element)){
+    _items.forEach(f);
+  }
+}
--- test/TestProperties.dart
+++ test/TestProperties.dart
@@ -24,6 +24,68 @@ class TestProperties implements IPropertyObject {
 
       });
 
+      test('with listeners', (){
+        var testProperty = new Property<String>("Test Property", null);
+
+        var object = new TestProperties();
+        expect(testProperty.get(object)).equals(null);
+        expect(testProperty.getCore(object)).equals(Property.Undefined);
+        expect(testProperty.isSet(object)).equals(false);
+
+        int v1 = 0;
+
+        // define handler 1
+        PropertyChangedHandler h1 = (obj, prop) {
+          v1++;
+        };
+        // set handler 1 on prop
+        testProperty.addHandler(object, h1);
+        expect(v1).equals(0);
+
+        // set prop
+        testProperty.set(object, "the foo!");
+        // - should fire
+        expect(v1).equals(1);
+
+        // clear prop
+        testProperty.clear(object);
+        // - should fire
+        expect(v1).equals(2);
+
+        // define handler 2 + set handler
+        int v2 = 0;
+
+        // define handler 1
+        PropertyChangedHandler h2 = (obj, prop) {
+          v2++;
+        };
+        // set handler 1 on prop
+        testProperty.addHandler(object, h2);
+        expect(v2).equals(0);
+
+        // set prop
+        testProperty.set(object, "the foo!");
+        // - should fire both handlers
+        expect(v1).equals(3);
+        expect(v2).equals(1);
+
+        // remove handler 1
+        testProperty.removeHandler(object, h1);
+        // clear prop
+        testProperty.clear(object);
+        // should fire h2, but not h1
+        expect(v1).equals(3);
+        expect(v2).equals(2);
+
+        // remove handler 2
+        testProperty.removeHandler(object, h2);
+        // set prop
+        testProperty.set(object, "the bar!");
+        // should not fire either handler
+        expect(v1).equals(3);
+        expect(v2).equals(2);        
+      });
+
       test('with default', (){
         var testProperty = new Property<int>("Test Property", 42);
 
