39224aa1c2a4b46878a17cc282cb5018002e137d
Kevin Moore <kevin@thinkpixellab.com>
2012-06-15 14:14:14 -0700

    added a 2-param action: Action2

--- lib/core/typedefs.dart
+++ lib/core/typedefs.dart
@@ -1,5 +1,6 @@
 typedef bool Predicate<T>(T obj);
-typedef T Func<T>();
 typedef void Action<T>(T param);
+typedef void Action2<T1, T2>(T1 param1, T2 param2);
+typedef T Func<T>();
 typedef TR Func1<T1, TR>(T1 param);
 typedef TR Func2<T1, T2, TR>(T1 param1, T2 param2);
