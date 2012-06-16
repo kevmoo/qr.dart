cb285382e5c04774e4d4db5d34bafad2e0b32103
Kevin Moore <kevin@thinkpixellab.com>
2012-06-16 16:51:52 -0700

    notes on _everything.dart

--- lib/_everything.dart
+++ lib/_everything.dart
@@ -2,7 +2,9 @@
  * _Nothing here. This library exists just to import the other libraries
  * to make documentation generation easy._
  * You should not import this file.
- * Instead, import the individual libraries as needed
+ * Instead, import the individual libraries as needed.
+ *
+ * Related dart bug [3555](http://code.google.com/p/dart/issues/detail?id=3555)
  */
 #library('dartlib_everything');
 
