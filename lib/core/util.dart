b55252d136d94a7c19284cb4128f227983493c59
Kevin Moore <kevin@thinkpixellab.com>
2012-06-18 20:17:36 -0700

    getHashCode is a bit sexier
    Thanks to Peter Ahé at Google

--- lib/core/util.dart
+++ lib/core/util.dart
@@ -1,10 +1,14 @@
 class Util {
   static int getHashCode(Iterable<Hashable> source) {
-    int current = 0;
+    int hash = 0;
     for (final h in source) {
       int next = h == null ? 0 : h.hashCode();
-      current = ((current << 5) + current) ^ next;
+      hash = 0x1fffffff & (hash + next);
+      hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+      hash ^= hash >> 6;
     }
-    return current;
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    hash ^= hash >> 11;
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
   }
 }
--- test/core/test_util.dart
+++ test/core/test_util.dart
@@ -10,18 +10,18 @@ class TestUtil {
     _hashCodeFun([], 0);
     _hashCodeFun([null], 0);
     _hashCodeFun([null, null], 0);
-    _hashCodeFun([1], 1);
-    _hashCodeFun([1,2], 35);
-    _hashCodeFun([2,1], 67);
-    _hashCodeFun(['foo'], 425588957);
-    _hashCodeFun([''], 1);
-    _hashCodeFun(['', ''], 32);
-    _hashCodeFun(['foo', 'bar'], 14526317940);
-    _hashCodeFun(['bar', 'foo'], 19989056244);
+    _hashCodeFun([1], 307143837);
+    _hashCodeFun([1,2], 93096440);
+    _hashCodeFun([2,1], 405401106);
+    _hashCodeFun(['foo'], 20484136);
+    _hashCodeFun([''], 307143837);
+    _hashCodeFun(['', ''], 313418812);
+    _hashCodeFun(['foo', 'bar'], 282245687);
+    _hashCodeFun(['bar', 'foo'], 247371737);
 
-    _hashCodeFun([null, 1], 1);
-    _hashCodeFun([null, null, 1], 1);
-    _hashCodeFun([1, null], 33);
+    _hashCodeFun([null, 1], 307143837);
+    _hashCodeFun([null, null, 1], 307143837);
+    _hashCodeFun([1, null], 15319219);
   }
 
   static void _hashCodeFun(Iterable<Hashable> items, int expectedValue) {
