972126a67df962e4331c2da6731bed5774d7074a
Kevin Moore <kevin@thinkpixellab.com>
2012-06-19 07:52:50 -0700

    GlobalId: now comparable, hashCode are more 'distributed'

--- lib/core/global_id.dart
+++ lib/core/global_id.dart
@@ -1,11 +1,17 @@
-class GlobalId implements Hashable{
+class GlobalId implements Hashable, Comparable {
   static int _globalId = 0;
-  final int _id;
-  
-  GlobalId(): _id = _globalId++;
-  
-  int hashCode(){
-    return _id;    
+  final int id;
+  final int _hashCode;
+
+  GlobalId._internal(int value) :
+    id = value,
+    _hashCode = Util.getHashCode([value]);
+
+  factory GlobalId() {
+    return new GlobalId._internal(_globalId++);
   }
-  
+
+  int compareTo(GlobalId other) => id.compareTo(other.id);
+
+  int hashCode() => _hashCode;
 }
