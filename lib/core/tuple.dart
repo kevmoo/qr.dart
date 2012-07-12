8ba797754e51479435a8f045dc6316538ca93049
Kevin Moore <kevin@thinkpixellab.com>
2012-07-12 10:20:16 -0700

    Tuple, Tuple3: implements Hashable
    Hopefully this doesn't annoy too many folks

--- lib/core/tuple.dart
+++ lib/core/tuple.dart
@@ -1,4 +1,5 @@
-class Tuple<T1, T2> {
+class Tuple<T1 extends Hashable, T2 extends Hashable>
+  implements Hashable {
   final T1 Item1;
   final T2 Item2;
 
@@ -9,9 +10,12 @@ class Tuple<T1, T2> {
   }
 
   String toString() => "Tuple: Item1: $Item1, Item2: $Item2";
+
+  int hashCode() => Util.getHashCode([Item1, Item2]);
 }
 
-class Tuple3<T1, T2, T3> extends Tuple<T1, T2> {
+class Tuple3<T1 extends Hashable, T2 extends Hashable, T3 extends Hashable>
+  extends Tuple<T1, T2> implements Hashable {
   final T3 Item3;
 
   const Tuple3(T1 param1, T2 param2, this.Item3) :
@@ -25,4 +29,6 @@ class Tuple3<T1, T2, T3> extends Tuple<T1, T2> {
   }
 
   String toString() => "Tuple3: Item1: $Item1, Item2: $Item2, Item3: $Item3";
+
+  int hashCode() => Util.getHashCode([Item1, Item2, Item3]);
 }
