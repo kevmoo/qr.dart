06cc08d2a5df3582ea16a580e87221d1854c91c8
Kevin Moore <kevin@thinkpixellab.com>
2012-07-16 12:26:29 -0700

    Mouse: leaned markMouseOver...
    exposes mouse over properties

--- lib/retained/mouse.dart
+++ lib/retained/mouse.dart
@@ -1,13 +1,40 @@
 class Mouse {
-  static final core.Property<bool> _isMouseOverProperty = const core.Property<bool>("IsMouseOver", false);
-  static final core.Property<bool> _isMouseDirectlyOverProperty = const core.Property<bool>("IsMouseDirectlyOver", false);
-  static final core.Property<bool> _stageMouseCacheProperty = const core.Property<bool>("_stageMouseCacheProperty");
+  static final core.Property<bool> isMouseOverProperty =
+      const core.Property<bool>("IsMouseOver", false);
+  static final core.Property<bool> isMouseDirectlyOverProperty =
+      const core.Property<bool>("IsMouseDirectlyOver", false);
+  static final core.Property<List<PElement>> _stageMouseCacheProperty =
+      const core.Property<List<PElement>>("_stageMouseCacheProperty");
 
   static bool isMouseOver(PElement element){
-    return _isMouseOverProperty.get(element);
+    return isMouseOverProperty.get(element);
   }
 
   static bool isMouseDirectlyOver(PElement element){
-    return _isMouseDirectlyOverProperty.get(element);
+    return isMouseDirectlyOverProperty.get(element);
+  }
+
+  static List<PElement> markMouseOver(Stage stage, [core.Coordinate coordinate = null]) {
+    core.requireArgumentNotNull(stage, 'stage');
+
+    final items = _stageMouseCacheProperty.get(stage);
+    if (items != null) {
+      items.forEach((e) {
+        isMouseOverProperty.clear(e);
+        isMouseDirectlyOverProperty.clear(e);
+      });
+    }
+    if (coordinate != null) {
+      var hits = RetainedUtil.hitTest(stage, coordinate);
+      _stageMouseCacheProperty.set(stage, hits);
+      hits.forEach((e) {
+        isMouseOverProperty.set(e, true);
+      });
+      if (hits.length > 0) {
+        isMouseDirectlyOverProperty.set(hits[0], true);
+      }
+      return hits;
+    }
+    return null;
   }
 }
