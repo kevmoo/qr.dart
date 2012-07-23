32e508453dc6b189c93d33df940067efb629b02a
Kevin Moore <kevin@thinkpixellab.com>
2012-07-23 14:52:51 -0700

    _everything: added missing imports
    added main() to make dartdoc happy -> w/ note about pending bug

--- lib/_everything.dart
+++ lib/_everything.dart
@@ -1,3 +1,5 @@
+#!/usr/bin/env dart --enable_type_checks --enable_asserts
+
 /**
  * _Nothing here. This library exists just to import the other libraries
  * to make documentation generation easy._
@@ -11,3 +13,14 @@
 #import('dartlib.dart');
 #import('test.dart');
 #import('retained.dart');
+#import('qr.dart');
+#import('async.dart');
+
+/**
+ * Does nothing. Just here to make `dartdoc` happy.
+ *
+ * Related dart bug [4145](http://code.google.com/p/dart/issues/detail?id=4145)
+ */
+void main() {
+  print('Main does nothing. Just here to make `dartdoc` happy.');
+}
