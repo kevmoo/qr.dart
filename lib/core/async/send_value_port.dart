3da78cb6d715627112d554fe796a51508f5827f5
Kevin Moore <kevin@thinkpixellab.com>
2012-07-22 18:50:20 -0700

    async/SendValuePort notes on exception handling badness in isolates

--- lib/core/async/send_value_port.dart
+++ lib/core/async/send_value_port.dart
@@ -1,3 +1,6 @@
+// NOTE: I'd love to not have to deal with this, but...
+//       it seems that unhandled exceptions (at least in Dartium) crash things
+//       http://dartbug.com/3734
 class SendValuePort<TInput, TOutput> {
   final Func1<TInput, TOutput> _func;
 
