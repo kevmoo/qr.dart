6270e0b93711c17c2f195180e33f6532acdfa849
Kevin Moore <kevin@thinkpixellab.com>
2012-08-06 16:10:34 -0500

    Action -> Action0: eliminate duplication w/ Action in unittest

--- lib/dartlib/typedefs.dart
+++ lib/dartlib/typedefs.dart
@@ -1,4 +1,4 @@
-typedef void Action();
+typedef void Action0();
 typedef void Action1<T>(T param);
 typedef void Action2<T1, T2>(T1 param1, T2 param2);
 typedef void Action3<T1, T2, T3>(T1 param1, T2 param2, T3 param3);
