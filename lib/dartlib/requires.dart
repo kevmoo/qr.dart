dabd6193b21752e457067ad64e88836ccc0b9923
Kevin Moore <kevin@thinkpixellab.com>
2012-08-12 22:13:59 -0400

    added 'require' -> shipping assert, basically

--- lib/dartlib/requires.dart
+++ lib/dartlib/requires.dart
@@ -1,7 +1,13 @@
+void require(bool truth, [String message = '']) {
+  if(!truth) {
+    throw new Exception(message);
+  }
+}
+
 void requireArgument(bool truth, [String arg = "", String message]) {
   if(!truth) {
     if(message != null) {
-      throw new DetailedIllegalArgumentException(arg, message);      
+      throw new DetailedIllegalArgumentException(arg, message);
     } else {
       throw new IllegalArgumentException(arg);
     }
