9473224e47e4168fd612d38fdd29e1bc9f0c1eae
Kevin Moore <kevin@thinkpixellab.com>
2012-08-18 14:10:44 -0400

    NullArgumentException: better toString

--- lib/dartlib/exceptions/null_argument_exception.dart
+++ lib/dartlib/exceptions/null_argument_exception.dart
@@ -1,3 +1,7 @@
+// TODO: hoping http://code.google.com/p/dart/issues/detail?id=4587 gets fixed
+//       so we don't need to create our own copy of arg and can just use super's
 class NullArgumentException extends IllegalArgumentException {
-  const NullArgumentException([String arg = ""]) : super(arg);
+  final String theArg;
+  const NullArgumentException([String arg = ""]) : this.theArg = arg, super(arg);
+  String toString() => "Null argument(s): $theArg";
 }
