fac2397219df7e1474d87ff4b90f1cdcfe21e0a1
Kevin Moore <kevin@thinkpixellab.com>
2012-08-18 10:23:44 -0400

    Shape: toString + whitespace cleanup

--- lib/retained/shape.dart
+++ lib/retained/shape.dart
@@ -6,19 +6,20 @@ class Shape extends PElement {
     : super(w, h);
 
   void drawOverride(CanvasRenderingContext2D ctx){
-      ctx.fillStyle = fillStyle;
-      // NOTE: warnings here will go away once dartbug.com/3342 is resolved
-      switch(shapeType){
-        case ShapeType.rect:
-          ctx.fillRect(0, 0, size.width, size.height);
-          break;
-        case ShapeType.ellipse:
-          CanvasUtil.ellipse(ctx, 0, 0, this.width, this.height);
-          ctx.fill();
-          break;
-        default:
-          throw "shape not known...";
-      }
+    ctx.fillStyle = fillStyle;
+    // NOTE: warnings here will go away once dartbug.com/3342 is resolved
+    switch(shapeType){
+      case ShapeType.rect:
+        ctx.fillRect(0, 0, size.width, size.height);
+        break;
+      case ShapeType.ellipse:
+        CanvasUtil.ellipse(ctx, 0, 0, this.width, this.height);
+        ctx.fill();
+        break;
+      default:
+        throw "shape not known...";
+    }
   }
 
+  String toString() => 'Shape [$fillStyle, ${shapeType.name}]';
 }
