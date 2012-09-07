36f7efc558b3d91ecc70fb60ddc744c5e000b151
Kevin Moore <kevin@thinkpixellab.com>
2012-09-06 22:55:26 -0400

    getMouseEventCoordinate -> moved back to event.offset[X|Y]
    thank you, shim

--- lib/html/html_util.dart
+++ lib/html/html_util.dart
@@ -1,19 +1,3 @@
 core.Coordinate getMouseEventCoordinate(MouseEvent event) {
-  final Element element = event.currentTarget;
-  assert(element != null);
-
-  final clientCoord = new core.Coordinate(event.clientX, event.clientY);
-  assert(clientCoord.isValid);
-
-  final elementOff = getElementCoordinate(element);
-  final shared = clientCoord - elementOff;
-
-  return shared;
-}
-
-core.Coordinate getElementCoordinate(Element element) {
-  final cr = element.$dom_getBoundingClientRect();
-  final crd = new core.Coordinate(cr.left.toInt(), cr.top.toInt());
-  assert(crd.isValid);
-  return crd;
+  return new core.Coordinate(event.offsetX, event.offsetY);
 }
