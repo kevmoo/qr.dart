e87111edd7b6484537a44eea3590bc839085a123
Kevin Moore <kevin@thinkpixellab.com>
2012-09-18 10:08:10 -0400

    expanding DARTBUG convention

--- lib/dartlib/exceptions/null_argument_exception.dart
+++ lib/dartlib/exceptions/null_argument_exception.dart
@@ -1,4 +1,4 @@
-// TODO: hoping http://code.google.com/p/dart/issues/detail?id=4587 gets fixed
+// DARTBUG: hoping http://code.google.com/p/dart/issues/detail?id=4587 gets fixed
 //       so we don't need to create our own copy of arg and can just use super's
 class NullArgumentException extends IllegalArgumentException {
   final String theArg;
