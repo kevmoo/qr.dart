80409b8b6781d4b68186541e566be0017d9f6b9d
Kevin Moore <kevin@thinkpixellab.com>
2012-09-23 16:09:00 -0400

    starting on work to send complex values over send ports

--- test/async/_async_runner.dart
+++ test/async/_async_runner.dart
@@ -7,11 +7,13 @@
 #import('../../lib/test.dart');
 
 #source('test_send_port_value.dart');
+#source('test_send_port_value_complex.dart');
 #source('test_future_value_result.dart');
 
 void runAsyncTests() {
   group('dartlib_async', (){
     TestSendPortValue.run();
     TestFutureValueResult.run();
+    TestSendPortValueComplex.run();
   });
 }
