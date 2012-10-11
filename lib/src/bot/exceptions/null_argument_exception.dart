92654e7cc2c33ca23b2a1b7f7227a7a041ad0d6e
Kevin Moore <kevin@thinkpixellab.com>
2012-10-11 11:20:12 -0400

    Fixed up NullArgumentException

--- lib/src/bot/exceptions/null_argument_exception.dart
+++ lib/src/bot/exceptions/null_argument_exception.dart
@@ -1,7 +1,6 @@
-// DARTBUG: hoping http://code.google.com/p/dart/issues/detail?id=4587 gets fixed
-//       so we don't need to create our own copy of arg and can just use super's
-class NullArgumentException extends IllegalArgumentException {
-  final String theArg;
-  const NullArgumentException([String arg = ""]) : this.theArg = arg, super(arg);
-  String toString() => "Null argument(s): $theArg";
+class NullArgumentException extends ArgumentError {
+  final String arg;
+  const NullArgumentException([String arg = '']) :
+    this.arg = arg, super('Null argument: $arg');
+  String toString() => "Null argument: $arg";
 }
