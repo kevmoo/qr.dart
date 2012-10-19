f30320bf774d277eded3fb766bd713746c0b54d0
Kevin Moore <kevin@thinkpixellab.com>
2012-10-19 14:01:02 -0400

    moved build script to 'hop'

--- bin/hop_runner.dart
+++ bin/hop_runner.dart
@@ -1,22 +1,68 @@
+#import('dart:io');
+#import('package:bot/bot.dart');
 #import('package:bot/hop.dart');
 #import('../test/console_test_harness.dart', prefix: 'test_console');
-#import('dart:io');
 
 void main() {
-  assertKnownPath();
+  _assertKnownPath();
 
   addAsyncTask('test', getTestRunner(test_console.testCore));
   addAsyncTask('docs', _compileDocs);
+  addAsyncTask('dart2js', _dart2jsAll);
   runHopCore();
 }
 
-Future<bool> _compileDocs(TaskContext state) {
-  assertKnownPath();
+Future<bool> _dart2jsAll(TaskContext state) {
+  final paths = $(['click', 'drag', 'fract', 'qr', 'spin'])
+      .map((d) => "example/$d/${d}_demo.dart")
+      .toList();
+  paths.add('test/browser_test_harness.dart');
 
+  final futureFuncs = $(paths).map((p) => () => _dart2js(state, p)).toList();
+
+  return _chainTasks(futureFuncs);
+}
+
+Future<bool> _chainTasks(List<Func<Future<bool>>> futures, [int index=0]) {
+  assert(futures.length > 0);
+  assert(index >= 0);
+  assert(index <= futures.length);
+  if(index == futures.length) {
+    return new Future.immediate(true);
+  }
+  final func = futures[index];
+  final future = func();
+  return future.chain((bool status) {
+    if(status) {
+      return _chainTasks(futures, index+1);
+    }
+    else {
+      return new Future.immediate(false);
+    }
+  });
+}
+
+Future<bool> _dart2js(TaskContext state, String file) {
+  _assertKnownPath();
+
+  final packageDir = new Directory('packages');
+  assert(packageDir.existsSync());
+
+  final args = ["--package-root=${packageDir.path}",
+                '--throw-on-error',
+                '-v',
+                "--out=${file}.js",
+                file];
+
+  return _runProcess(state, 'dart2js', args);
+}
+
+Future<bool> _compileDocs(TaskContext state) {
+  _assertKnownPath();
   return _runProcess(state, "bin/compile_docs", []);
 }
 
-void assertKnownPath() {
+void _assertKnownPath() {
   // since there is no way to determine the path of 'this' file
   // assume that Directory.current() is the root of the project.
   // So check for existance of /bin/hop_runner.dart
