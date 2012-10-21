be30bca06981ff373607789b90a3d3fd674bc416
Kevin Moore <kevin@thinkpixellab.com>
2012-10-21 18:55:15 -0400

    moved dart2js and process running code to shared library

--- bin/hop_runner.dart
+++ bin/hop_runner.dart
@@ -8,58 +8,22 @@ void main() {
 
   addAsyncTask('test', getTestRunner(test_console.testCore));
   addAsyncTask('docs', _compileDocs);
-  addAsyncTask('dart2js', _dart2jsAll);
-  runHopCore();
-}
 
-Future<bool> _dart2jsAll(TaskContext state) {
+  //
+  // Dart2js
+  //
   final paths = $(['click', 'drag', 'fract', 'qr', 'spin'])
       .map((d) => "example/$d/${d}_demo.dart")
       .toList();
   paths.add('test/browser_test_harness.dart');
 
-  final futureFuncs = $(paths).map((p) => () => _dart2js(state, p)).toList();
-
-  return _chainTasks(futureFuncs);
-}
-
-Future<bool> _chainTasks(List<Func<Future<bool>>> futures, [int index=0]) {
-  assert(futures.length > 0);
-  assert(index >= 0);
-  assert(index <= futures.length);
-  if(index == futures.length) {
-    return new Future.immediate(true);
-  }
-  final func = futures[index];
-  final future = func();
-  return future.chain((bool status) {
-    if(status) {
-      return _chainTasks(futures, index+1);
-    }
-    else {
-      return new Future.immediate(false);
-    }
-  });
-}
-
-Future<bool> _dart2js(TaskContext state, String file) {
-  _assertKnownPath();
-
-  final packageDir = new Directory('packages');
-  assert(packageDir.existsSync());
-
-  final args = ["--package-root=${packageDir.path}",
-                '--throw-on-error',
-                '-v',
-                "--out=${file}.js",
-                file];
-
-  return _runProcess(state, 'dart2js', args);
+  addAsyncTask('dart2js', getDart2jsTask(paths));
+  runHopCore();
 }
 
 Future<bool> _compileDocs(TaskContext state) {
   _assertKnownPath();
-  return _runProcess(state, "bin/compile_docs", []);
+  return runProcess(state, "bin/compile_docs", []);
 }
 
 void _assertKnownPath() {
@@ -68,33 +32,4 @@ void _assertKnownPath() {
   // So check for existance of /bin/hop_runner.dart
   final thisFile = new File('bin/hop_runner.dart');
   assert(thisFile.existsSync());
-}
-
-Future<bool> _runProcess(TaskContext state, String command, List<String> args) {
-  final completer = new Completer<bool>();
-  final process = Process.start(command, args);
-
-  final standardOutput = new ListOutputStream();
-  standardOutput.onData = () {
-    final data = standardOutput.read();
-    assert(data != null);
-    final str = new String.fromCharCodes(data).trim();
-    state.fine(str);
-  };
-  process.stdout.pipe(standardOutput, true);
-
-  final errorOutput = new ListOutputStream();
-  errorOutput.onData = () {
-    final data = standardOutput.read();
-    assert(data != null);
-    final str = new String.fromCharCodes(data).trim();
-    state.error(str);
-  };
-  process.stderr.pipe(errorOutput, true);
-
-  process.onExit = (int exitCode){
-    completer.complete(exitCode == 0);
-  };
-
-  return completer.future;
 }
--- lib/hop.dart
+++ lib/hop.dart
@@ -3,6 +3,7 @@
 // TODO: documentation for tasks
 // TODO: formalize print/log model
 
+#import('dart:io');
 #import('dart:isolate');
 #import('package:args/args.dart');
 #import('package:unittest/unittest.dart');
@@ -29,4 +30,76 @@ void addTask(String name, Action1<TaskContext> execFunc) {
 
 void addAsyncTask(String name, Func1<TaskContext, Future<bool>> execFuture) {
   _sharedState.addTaskAsync(name, execFuture);
+}
+
+Future<bool> runProcess(TaskContext state, String command, List<String> args) {
+  final completer = new Completer<bool>();
+  final process = Process.start(command, args);
+
+  final standardOutput = new ListOutputStream();
+  standardOutput.onData = () {
+    final data = standardOutput.read();
+    assert(data != null);
+    final str = new String.fromCharCodes(data).trim();
+    state.fine(str);
+  };
+  process.stdout.pipe(standardOutput, true);
+
+  final errorOutput = new ListOutputStream();
+  errorOutput.onData = () {
+    final data = standardOutput.read();
+    assert(data != null);
+    final str = new String.fromCharCodes(data).trim();
+    state.error(str);
+  };
+  process.stderr.pipe(errorOutput, true);
+
+  process.onExit = (int exitCode){
+    completer.complete(exitCode == 0);
+  };
+
+  return completer.future;
+}
+
+Func1<TaskContext, Future<bool>> getDart2jsTask(List<String> inputs) {
+  return (context) {
+    final futureFuncs = $(inputs).map((p) => () => _dart2js(context, p)).toList();
+    return _chainTasks(futureFuncs);
+  };
+}
+
+Future<bool> _chainTasks(List<Func<Future<bool>>> futures, [int index=0]) {
+  assert(futures.length > 0);
+  assert(index >= 0);
+  assert(index <= futures.length);
+  if(index == futures.length) {
+    return new Future.immediate(true);
+  }
+  final func = futures[index];
+  final future = func();
+  return future.chain((bool status) {
+    if(status) {
+      return _chainTasks(futures, index+1);
+    }
+    else {
+      return new Future.immediate(false);
+    }
+  });
+}
+
+Future<bool> _dart2js(TaskContext state, String file, [String output = null]) {
+  if(output == null) {
+    output = "${file}.js";
+  }
+
+  final packageDir = new Directory('packages');
+  assert(packageDir.existsSync());
+
+  final args = ["--package-root=${packageDir.path}",
+                '--throw-on-error',
+                '-v',
+                "--out=$output",
+                file];
+
+  return runProcess(state, 'dart2js', args);
 }
