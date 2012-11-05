0315383ff44eadb321b9980dd4cdc22131f0ecf9
Kevin Moore <kevin@thinkpixellab.com>
2012-11-04 20:39:31 -0500

    stubbing out actual tests

---
+++ test/_basic_sync_tests.dart
@@ -0,0 +1,9 @@
+part of console_test_harness;
+
+class SyncTests {
+  static void run() {
+    test('true result is cool', pending);
+    test('false result fails', pending);
+    test('null result is sad', pending);
+  }
+}
--- test/console_test_harness.dart
+++ test/console_test_harness.dart
@@ -2,6 +2,9 @@ library console_test_harness;
 
 import 'package:unittest/unittest.dart';
 import 'package:unittest/vm_config.dart';
+import 'package:bot/test.dart';
+
+part '_basic_sync_tests.dart';
 
 main() {
   final config = new VmConfiguration();
@@ -12,7 +15,5 @@ void testCore(Configuration config) {
   configure(config);
   groupSep = ' - ';
 
-  test('hello, world', () {
-    expect(true, true);
-  });
+  group('sync tests', SyncTests.run);
 }
