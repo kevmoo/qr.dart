282416905d280d9e0d332fc3bb8fadc4ce4706ab
Kevin Moore <kevin@thinkpixellab.com>
2012-11-07 22:32:57 -0500

    dartdoc execution is done via dart, not compile_docs bash script

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -24,7 +24,43 @@ void main() {
 
 Future<bool> _compileDocs(TaskContext state) {
   _assertKnownPath();
-  return startProcess(state, "tool/compile_docs", []);
+  return _getLibs().chain((libs) {
+    final args = ['--omit-generation-time', '--out', 'build/doc', '--verbose'];
+    args.addAll(libs);
+    return startProcess(state, "dartdoc", args);
+  });
+}
+
+Future<List<String>> _getLibs() {
+  final completer = new Completer<List<String>>();
+
+  final lister = new Directory('lib').list();
+  final libs = new List<String>();
+
+  lister.onFile = (String file) {
+    if(file.endsWith('.dart')) {
+      // excluded test because of issues with dartdoc and sdk libs
+      // DARTBUG: http://code.google.com/p/dart/issues/detail?id=5460
+      // in this case, unittest
+      if(!file.endsWith('test.dart')) {
+        libs.add(file);
+      }
+    }
+  };
+
+  lister.onDone = (bool done) {
+    if(done) {
+      completer.complete(libs);
+    } else {
+      completer.completeException('did not finish');
+    }
+  };
+
+  lister.onError = (error) {
+    completer.completeException(error);
+  };
+
+  return completer.future;
 }
 
 void _assertKnownPath() {
