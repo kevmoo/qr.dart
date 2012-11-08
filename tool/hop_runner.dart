f01f8c655402407f493c40bc737a95f09d937d69
Kevin Moore <kevin@thinkpixellab.com>
2012-11-07 22:40:56 -0500

    moving dartdoc task impl into its own file

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -1,8 +1,12 @@
+library hop_runner;
+
 import 'dart:io';
 import 'package:bot/bot.dart';
 import 'package:hop/hop.dart';
 import 'package:hop/tasks.dart';
 import '../test/console_test_harness.dart' as test_console;
+
+part 'dartdoc.dart';
 
 void main() {
   _assertKnownPath();
@@ -20,47 +24,6 @@ void main() {
 
   addAsyncTask('dart2js', createDart2JsTask(paths));
   runHopCore();
-}
-
-Future<bool> _compileDocs(TaskContext state) {
-  _assertKnownPath();
-  return _getLibs().chain((libs) {
-    final args = ['--omit-generation-time', '--out', 'build/doc', '--verbose'];
-    args.addAll(libs);
-    return startProcess(state, "dartdoc", args);
-  });
-}
-
-Future<List<String>> _getLibs() {
-  final completer = new Completer<List<String>>();
-
-  final lister = new Directory('lib').list();
-  final libs = new List<String>();
-
-  lister.onFile = (String file) {
-    if(file.endsWith('.dart')) {
-      // excluded test because of issues with dartdoc and sdk libs
-      // DARTBUG: http://code.google.com/p/dart/issues/detail?id=5460
-      // in this case, unittest
-      if(!file.endsWith('test.dart')) {
-        libs.add(file);
-      }
-    }
-  };
-
-  lister.onDone = (bool done) {
-    if(done) {
-      completer.complete(libs);
-    } else {
-      completer.completeException('did not finish');
-    }
-  };
-
-  lister.onError = (error) {
-    completer.completeException(error);
-  };
-
-  return completer.future;
 }
 
 void _assertKnownPath() {
