7dbbd5efddf76c0f2ba8b308adb092311641eabf
Kevin Moore <kevin@thinkpixellab.com>
2012-11-07 19:38:24 -0500

    IntegrationTests: don't assume 'hop' is on the path
    use 'bin/hop'

--- test/hop/integration_tests.dart
+++ test/hop/integration_tests.dart
@@ -1,6 +1,8 @@
 part of test_hop;
 
 class IntegrationTests {
+  static final hopPath = 'bin/hop';
+
   static void run() {
     test('hop output is sorted', _testOutputSorted);
     test('bad hop command', _testBadHopCommand);
@@ -8,23 +10,17 @@ class IntegrationTests {
 
   static void _testBadHopCommand() {
     final onComplete = expectAsync1((Future<io.ProcessResult> f) {
-      if(!f.hasValue) {
-        print(f.exception);
-      }
       expect(f.hasValue, isTrue);
       final pr = f.value;
       expect(pr.exitCode, equals(EXIT_CODE_USAGE));
     });
 
-    final f = io.Process.run('hop', ['bad_command_name']);
+    final f = io.Process.run(hopPath, ['bad_command_name']);
     f.onComplete(onComplete);
   }
 
   static void _testOutputSorted() {
     final onComplete = expectAsync1((Future<io.ProcessResult> f) {
-      if(!f.hasValue) {
-        print(f.exception);
-      }
       expect(f.hasValue, isTrue);
       final pr = f.value;
       expect(pr.exitCode, equals(EXIT_CODE_SUCCESS));
@@ -32,7 +28,7 @@ class IntegrationTests {
       expect(lines, orderedEquals(['about', 'docs', 'test']));
     });
 
-    final f = io.Process.run('hop', [Runner.RAW_TASK_LIST_CMD]);
+    final f = io.Process.run(hopPath, [Runner.RAW_TASK_LIST_CMD]);
     f.onComplete(onComplete);
   }
 }
