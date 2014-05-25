08538e270f8636b26d7a54b8048b70956d8de7d3
Kevin Moore <github@j832.com>
2014-05-25 00:03:07 -0700

    clean up tools

--- tool/src/pages.dart
+++ tool/src/pages.dart
@@ -1,9 +1,9 @@
-
-library json_pretty.tool.pages;
+library qr.tool.pages;
 
 import 'dart:async';
 import 'dart:io';
-import 'package:bot/bot.dart';
+
+import 'package:bot_io/bot_io.dart';
 import 'package:git/git.dart';
 import 'package:hop/hop.dart';
 import 'package:path/path.dart' as p;
@@ -33,46 +33,3 @@ Future _populateBranch(Directory dir) {
 }
 
 const _TARGET_BRANCH = 'gh-pages';
-
-/// Stolen from [hop_docgen].
-Future copyDirectory(String sourceDirectory, String targetDir) {
-  requireArgument(FileSystemEntity.isDirectorySync(sourceDirectory),
-      'sourceDirectory', 'Must exist');
-  _requireEmptyDir(targetDir, 'targetDir');
-
-  var dir = new Directory(sourceDirectory);
-
-  return streamForEachAsync(dir.list(recursive: true, followLinks: false),
-      (fse) => _copyItem(fse, sourceDirectory, targetDir));
-}
-
-/// Stolen from [hop_docgen].
-dynamic _copyItem(FileSystemEntity fse, String source, String target) {
-  if(fse is Directory) return null;
-  if(fse is Link) {
-    throw new ArgumentError('Cannot rock on the link at ${fse.path}');
-  }
-
-  var relative = p.relative(fse.path, from: source);
-
-  var newPath = p.join(target, relative);
-
-  var parentDirPath = p.dirname(newPath);
-
-  var parentDir = new Directory(parentDirPath);
-
-  return parentDir.create(recursive: true).then((_) {
-    return (fse as File).copy(newPath);
-  });
-}
-
-/// Stolen from [hop_docgen].
-void _requireEmptyDir(String path, String argName) {
-  requireArgument(FileSystemEntity.isDirectorySync(path), argName,
-      '$path does not exist or is not a directory.');
-
-  var dir = new Directory(path);
-
-  requireArgument(dir.listSync().isEmpty, argName,
-      '$path is not empty.');
-}
