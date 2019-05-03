5269eaa76d5bdc97e9dca4d7fec66eeaeb67c3bc
Peter Chung <touhonoob@gmail.com>
2019-05-04 02:35:31 +0800

    Add assertions to qr_code_test

--- test/qr_code_test.dart
+++ test/qr_code_test.dart
@@ -1,20 +1,42 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:qr/qr.dart';
 import 'package:test/test.dart';
 
 void main() {
+  var data = jsonDecode(File('test/qr_code_test.json').readAsStringSync());
+
   test('simple', () {
-    for (var typeNumber = 1; typeNumber <= 10; typeNumber++) {
+    for (var typeNumber = 1; typeNumber <= 40; typeNumber++) {
       for (var quality in QrErrorCorrectLevel.levels) {
-        QrCode(typeNumber, quality)
+        var qr = QrCode(typeNumber, quality)
           ..addData('shanna!')
           ..make();
+        for (var i = 0; i < qr.modules.length; i++) {
+          expect(encodeBoolListToString(qr.modules[i]),
+              data[typeNumber.toString()][quality.toString()][i]);
+        }
       }
     }
   });
 
   test('fromData', () {
     for (var quality in QrErrorCorrectLevel.levels) {
-      QrCode.fromData(data: 'shanna!', errorCorrectLevel: quality).make();
+      var qr = QrCode.fromData(data: 'shanna!', errorCorrectLevel: quality)
+        ..make();
+      for (var i = 0; i < qr.modules.length; i++) {
+        expect(encodeBoolListToString(qr.modules[i]),
+            data[1.toString()][quality.toString()][i]);
+      }
     }
   });
+}
+
+String encodeBoolListToString(List<bool> l) {
+  var r = '';
+  for (var i = 0; i < l.length; i++) {
+    r += l[i] ? '1' : '0';
+  }
+  return r;
 }
