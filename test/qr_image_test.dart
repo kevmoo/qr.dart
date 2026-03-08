b7edc29ae9a61511d3dfd1f7b0e84bd44cac6657
kevmoo <kevmoo@google.com>
2026-03-08 14:47:13 -0700

    fixes

--- test/qr_image_test.dart
+++ test/qr_image_test.dart
@@ -2,34 +2,39 @@ import 'package:qr/qr.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('QrImage', () {
-    late QrImage qrImage;
+  late QrImage qrImage;
+  late int moduleCount;
+
+  setUpAll(() {
     const typeNumber = 1;
     final qrCode = QrCode(typeNumber, QrErrorCorrectLevel.low)..addData('test');
     qrImage = QrImage(qrCode);
-    final moduleCount = qrImage.moduleCount;
+    moduleCount = qrImage.moduleCount;
+  });
 
-    group('isDark', () {
-      test('should throw RangeError when row is less than 0', () {
-        expect(() => qrImage.isDark(-1, 0), throwsRangeError);
-      });
+  test('should throw RangeError when row is less than 0', () {
+    expect(() => qrImage.isDark(-1, 0), throwsRangeError);
+  });
 
-      test('should throw RangeError when row is greater than or equal to moduleCount', () {
-        expect(() => qrImage.isDark(moduleCount, 0), throwsRangeError);
-      });
+  test('should throw RangeError when row is greater than '
+      'or equal to moduleCount', () {
+    expect(() => qrImage.isDark(moduleCount, 0), throwsRangeError);
+  });
 
-      test('should throw RangeError when col is less than 0', () {
-        expect(() => qrImage.isDark(0, -1), throwsRangeError);
-      });
+  test('should throw RangeError when col is less than 0', () {
+    expect(() => qrImage.isDark(0, -1), throwsRangeError);
+  });
 
-      test('should throw RangeError when col is greater than or equal to moduleCount', () {
-        expect(() => qrImage.isDark(0, moduleCount), throwsRangeError);
-      });
+  test('should throw RangeError when col is greater than '
+      'or equal to moduleCount', () {
+    expect(() => qrImage.isDark(0, moduleCount), throwsRangeError);
+  });
 
-      test('should not throw when row and col are within valid range', () {
-        expect(() => qrImage.isDark(0, 0), returnsNormally);
-        expect(() => qrImage.isDark(moduleCount - 1, moduleCount - 1), returnsNormally);
-      });
-    });
+  test('should not throw when row and col are within valid range', () {
+    expect(() => qrImage.isDark(0, 0), returnsNormally);
+    expect(
+      () => qrImage.isDark(moduleCount - 1, moduleCount - 1),
+      returnsNormally,
+    );
   });
 }
